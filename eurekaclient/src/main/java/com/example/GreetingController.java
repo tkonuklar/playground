490a660228228359161b0a4debf232f2479e1227
Tugce Konuklar <tugce.konuklar@infinitec.solutions>
2019-01-15 01:44:13 +0100

    first commit

---
+++ eurekaclient/src/main/java/com/example/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return String.format("Hello from Eureka Client : %s", eurekaClient.getApplication(appName).getName());
+    }
+}
